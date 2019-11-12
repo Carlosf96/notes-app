bc6cdb43308b3f20fb017e397451a69d4ea70ea5
carlosf96 <carlos.f.roque96@gmail.com>
2019-11-12 17:16:24 -0600

    Fetch request to get all users works

---
+++ frontend/src/fetchService.js
@@ -0,0 +1,19 @@
+document.body.onload = loadNotes;
+
+var loadNotes = (async (req, res) => {
+    var res = await window.fetch('http://localhost:8000/api/notes/');
+    console.log(res, 'this is the results')
+    var data = await res.json();
+    Promise.resolve(data);
+    console.log(data, 'raw data');
+    data.map(note=>{
+    var list = document.getElementById('list')
+    var newNote = document.createElement('li');
+    var noteContent = document.createElement('input');
+    // noteContent.placeholder = note.noteTitle;
+    var noteContent2 = document.createElement('input');
+    note.placeholder = note.noteBody;
+    newNote.appendChild(noteContent)
+    list.appendChild(newNote);
+  })
+})()
