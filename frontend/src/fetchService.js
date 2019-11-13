a7ffae794aae30facbef0532ba6ab56a852b76df
carlosf96 <carlos.f.roque96@gmail.com>
2019-11-13 09:45:11 -0600

    sticky note almost done, need to find good font

--- frontend/src/fetchService.js
+++ frontend/src/fetchService.js
@@ -1,18 +1,22 @@
 document.body.onload = loadNotes;
 
 var loadNotes = (async (req, res) => {
-    var res = await window.fetch('http://localhost:8000/api/notes/');
-    var data = await res.json();
-    Promise.resolve(data);
-    console.log(data, 'raw data');
-    data.map(note=>{
-    var list = document.getElementById('list')
-    var newNote = document.createElement('li');
-    var noteContent = document.createElement('input');
-    // noteContent.placeholder = note.noteTitle;
-    var noteContent2 = document.createElement('input');
-    note.placeholder = note.noteBody;
-    newNote.appendChild(noteContent)
+  var res = await window.fetch("http://localhost:8000/api/notes/");
+  var data = await res.json();
+  Promise.resolve(data);
+  console.log(data, "raw data");
+  data.notes.map(note => {
+    var list = document.getElementById("list");
+    var newNote = document.createElement("li");
+    var noteContent = document.createElement("input");
+    noteContent.placeholder = note.noteTitle;
+    var noteContent2 = document.createElement("input");
+    noteContent2.placeholder = note.noteBody;
+    var xButton = document.createElement("button");
+    xButton.innerText = "X";
+    newNote.appendChild(xButton);
+    newNote.appendChild(noteContent);
+    newNote.appendChild(noteContent2);
     list.appendChild(newNote);
-  })
-})()
+  });
+})();
