9cb7fab6bcb37e73033729ab6880cde65419ee63
carlosf96 <carlos.f.roque96@gmail.com>
2019-11-13 11:19:33 -0600

    Refactoring code to look cleaner and better

--- frontend/src/fetchService.js
+++ frontend/src/fetchService.js
@@ -4,37 +4,39 @@ var loadNotes = (async (req, res) => {
   var res = await window.fetch("http://localhost:8000/api/notes/");
   var data = await res.json();
   Promise.resolve(data);
-  console.log(data, "raw data");
   createNoteComponent(data);
-  document.getElementById('cool-button').onclick = renderNewNote;
+  document.getElementById("add-button").onclick = createNewNote;
+  // document.getElementById("delete-button").onclick = deleteNote;
 })();
 
-var createNoteComponent = (data) => {
-  data.notes.map(note => {
-    var list = document.getElementById("list");
-    var newNote = document.createElement("li");
-    var noteContent = document.createElement("input");
-    noteContent.placeholder = note.noteTitle;
-    var noteContent2 = document.createElement("input");
-    noteContent2.placeholder = note.noteBody;
-    var xButton = document.createElement("button");
-    xButton.innerText = "X";
-    newNote.appendChild(xButton);
-    newNote.appendChild(noteContent);
-    newNote.appendChild(noteContent2);
-    list.appendChild(newNote);
-  });
-} 
+var createNoteComponent = ({ notes }) => notes.map(note => createNewNote(note));
 
-var renderNewNote = () => {
+var createNewNote = note => {
   var list = document.getElementById("list");
   var newNote = document.createElement("li");
   var noteContent = document.createElement("input");
   var noteContent2 = document.createElement("input");
   var xButton = document.createElement("button");
+  if(!note) {
+    noteContent.placeholder = note.noteTitle 
+    noteContent2.placeholder = note.noteBody;
+  } else {
+    noteContent.placeholder = 'Title';
+    noteContent2.placeholder = 'Body'
+  }
   xButton.innerText = "X";
+  xButton.setAttribute("id", "delete-button");
   newNote.appendChild(xButton);
   newNote.appendChild(noteContent);
   newNote.appendChild(noteContent2);
   list.appendChild(newNote);
-}
+};
+
+var deleteNote = async (req, res) => {
+  var res = await window.fetch("http://localhost:8000/api/notes/:id", {
+    method: "DELETE"
+  });
+  var data = await res.json();
+  Promise.resolve(data);
+  console.log("note has been deleted");
+};
