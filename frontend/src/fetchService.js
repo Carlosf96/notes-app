f8eec1e0c7d5861d23aafaac6d1ce8514528c950
carlosf96 <carlos.f.roque96@gmail.com>
2019-11-14 15:17:20 -0600

    Refactored code again

--- frontend/src/fetchService.js
+++ frontend/src/fetchService.js
@@ -1,37 +1,52 @@
 document.body.onload = loadNotes;
 
+const newNote = note => {
+  return `
+   <li class='list-item'>
+    <form class='input-forms'>
+    <div class='note-title'>
+    <button class='delete-button' onclick="deleteNote()">x</button>
+    <input class='title-input' placeholder=${note ? note.noteTitle : "Title"}>
+    </div>
+    <input class='body-input' placeholder=${note ? note.noteBody : "Body"}>
+    </form>
+   </li> 
+`;
+};
+
 var loadNotes = (async () => {
-  var res = await window.fetch("http://localhost:8000/api/notes/");
-  var data = await res.json();
+  const res = await window.fetch("http://localhost:8000/api/notes/");
+  const data = await res.json();
   Promise.resolve(data);
   createNoteComponent(data);
-  document.getElementById("add-button").onclick = createNewNote;
-  document.getElementById("delete-button").onclick = deleteNote;
   return data;
 })();
 
-// const { notes } = loadNotes;
+var updateNote = async (note) => {
+  const res = await window.fetch(`http://localhost:8000/api/notes/${id}`, {
+    method: "Put",
+    body: note
+  });
+  const data = await res.json();
+  Promise.resolve(data);
+  console.log("note has been updated")
+}
 
-var createNoteComponent = ({ notes }) => notes.map(note => createNewNote(note));
-
-var createNewNote = note => {
- const list = document.getElementById('list');
- let li = `
-   <li class='list-item'>
-     <button id='delete-button'>x</button>
-     <input placeholder=${note.noteTitle || 'Title'}>
-     <input placeholder=${note.noteBody || 'Body'}>
-   </li> 
-`;
- list.insertAdjacentHTML('afterend', li);
-}         
-
-
-var deleteNote = async (id) => {
-  var res = await window.fetch(`http://localhost:8000/api/notes/${id}`, {
+var deleteNote = async id => {
+  const res = await window.fetch(`http://localhost:8000/api/notes/${id}`, {
     method: "DELETE"
   });
-  var data = await res.json();
+  const data = await res.json();
   Promise.resolve(data);
   console.log("note has been deleted");
 };
+
+// const { notes } = loadNotes;
+
+var addToList = li => {
+  let list = document.getElementById("list");
+  list.insertAdjacentHTML("beforeend", li);
+};
+
+var createNoteComponent = ({ notes }) => notes.map(note => addToList(newNote(note)));
+var createNewNoteComponent = () => addToList(newNote());
