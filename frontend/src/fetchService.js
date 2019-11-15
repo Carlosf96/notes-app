519541f056fd5a1719fe49f7a890a06ec860f048
Carlosf96 <carlos.f.roque96@gmail.com>
2019-11-15 03:10:12 -0600

    Delete functionality now works

--- frontend/src/fetchService.js
+++ frontend/src/fetchService.js
@@ -1,7 +1,6 @@
 const FetchNoteService = () => {
   const createNote = async note => {
-    const res = await window.fetch("http://localhost:8000/api/notes/",
-    {
+    const res = await window.fetch('http://localhost:8000/api/notes/', {
       method: 'POST',
       body: note
     });
@@ -10,41 +9,31 @@ const FetchNoteService = () => {
     return Promise.resolve(data);
   };
   const getNotes = async () => {
-    const res = await window.fetch("http://localhost:8000/api/notes/");
+    const res = await window.fetch('http://localhost:8000/api/notes/');
     const data = await res.json();
     console.log(data, 'Retrieved notes');
     return Promise.resolve(data);
   };
-  const saveNote = async note => {
-    const res = await window.fetch("http://localhost:8000/api/notes/", {
-      method: "POST",
-      body: note
-    });
-    const data = await res.json();
-    console.log("Note has been saved");
-    return Promise.resolve(data);
-  };
-  const updateNote = async note => {
+  const updateNote = async (id, note) => {
     const res = await window.fetch(`http://localhost:8000/api/notes/${id}`, {
-      method: "PUT",
+      method: 'PUT',
       body: note
     });
     const data = await res.json();
-    console.log("note has been updated");
+    console.log('note has been updated');
     return Promise.resolve(data);
   };
   const deleteNote = async id => {
     const res = await window.fetch(`http://localhost:8000/api/notes/${id}`, {
-      method: "DELETE"
+      method: 'DELETE'
     });
     const data = await res.json();
-    console.log("note has been deleted");
+    console.log('note has been deleted');
     return Promise.resolve(data);
   };
   return {
     createNote,
     getNotes,
-    saveNote,
     updateNote,
     deleteNote
   };
--- frontend/src/renderView.js
+++ frontend/src/renderView.js
@@ -1,33 +1,47 @@
-const createNewNote = note => {
-  return `
+const createNewNote = note => `
   <li class='list-item' id=${note.id || 'single-note'}>
-    <form class='input-forms'>
+    <form class='input-forms' onsubmit="saveNoteContent()">
       <div class='note-title'>
-        <button class='delete-button' id=${note.id+'1'} onclick="deleteAndRemoveFromList()">x</button>
-        <input class='title-input' placeholder=${
-          note.noteTitle ? note.noteTitle : 'Title'
-        }>
+        <i type='text' class='delete-button' id=${note.id +
+          '1'} onclick="deleteAndRemoveFromList()">x</i>
+        <input class='title-input' id=${note.id + '-title'} placeholder=${
+  note.noteTitle ? note.noteTitle : 'Title'
+}>
       </div>
-      <input class='body-input' placeholder=${note.noteBody ? note.noteBody : 'Body'}>
+      <input class='body-input' id=${note.id + '-body'} placeholder=${
+  note.noteBody ? note.noteBody : 'Body'
+}>
     </form>
   </li> 
   `;
-};
 const createAndAddToList = () => {
   FetchNoteService()
     .createNote({
       noteTitle: '',
       noteBody: ''
     })
-    .then(res => addToList(createNewNote(res.note)));
+    .then(res => addToList(createNewNote(res.note)))
+    .catch(err => console.log(err));
 };
+
 const deleteAndRemoveFromList = () => {
-  const notTheID = event.srcElement.id.split('');
-  notTheID.pop();
-  const theId = notTheID.join('');
-  console.log(notTheID,theId)
-  FetchNoteService().deleteNote(theId).then(res => console.log(res));
-}
+  const notTheId = event.srcElement.id.split('');
+  notTheId.pop();
+  const theId = notTheId.join('');
+  FetchNoteService()
+    .deleteNote(theId)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+  removeFromList(theId);
+};
+
+const saveNoteContent = (id, note) => {
+  event.preventDefault();
+  FetchNoteService()
+    .updateNote(id, note)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+};
 const getList = () => document.getElementById('list');
 const addToList = li => getList().insertAdjacentHTML('beforeend', li);
 const removeFromList = id => document.getElementById(id).remove();
