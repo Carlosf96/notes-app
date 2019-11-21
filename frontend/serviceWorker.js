4b2001c1dc56859a1f48d14cc65ac123319989b9
carlosf96 <carlos.f.roque96@gmail.com>
2019-11-21 12:09:34 -0600

    removed link to SW form html and now registering directly from HTML

--- frontend/serviceWorker.js
+++ frontend/serviceWorker.js
@@ -1,13 +1,3 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/frontend/serviceWorker.js', {
-      scope: './'
-    })
-    .then(reg => {
-      console.log('Service worker has been registered for scope:' + reg.scope);
-    })
-    .catch(err => console.log(err));
-};
 const staticAssets = [
   './',
   './public/Notes.css',
