7b8990367248887d2ceefe8e65230baa04ec8fc7
carlosf96 <carlos.f.roque96@gmail.com>
2019-11-21 17:25:21 -0600

    refactored controllers to query db and send res to frontend

--- backend/controllers/Notes.controller.js
+++ backend/controllers/Notes.controller.js
@@ -1,42 +1,50 @@
 const cuid = require("cuid");
-let notes = [];
+const Note = require("../models").Note;
+
 module.exports.getAllNotes = (req, res) => {
-  return res.status(200).json({
-    notes,
-  });
+  return Note.findAll()
+    .then(notes => res.status(200).json({
+      notes
+    }))
+    .catch(err => console.log(err));
 };
 module.exports.updateNote = async (req, res) => {
   const { id } = req.params;
-  const { noteTitle, noteBody } = req.body;
-  const noteIndex = notes.findIndex(note => note.id === id);
-  if (noteIndex < 0) {
-    return res.status(400).json({});
-  }
-  notes[noteIndex] = {
-    id,
-    noteTitle,
-    noteBody,
-  };
-  res.status(200).json({
-    note: notes[noteIndex]
-  });
-  console.log(notes);
+  const { title, body } = req.body;
+  Note.update({
+      title: title,
+      body: body
+    }, {
+      where: {
+        id: id
+      }
+    })
+    .then(() => res.status(200).json({
+      message: "Note has been updated"
+    }))
+    .catch(err => console.log(err));
 };
 module.exports.createNote = (req, res) => {
   const note = req.body;
   const newNote = {
     id: cuid(),
-    ...note,
+    ...note
   };
-  notes.push(newNote);
-  res.status(201).json({ note: newNote });
+  Note.create({
+      ...newNote
+    })
+    .then(() => res.status(201).json({
+      note: newNote
+    }))
+    .catch(err => console.log(err));
 };
-module.exports.deleteNote =  (req, res) => {
+module.exports.deleteNote = (req, res) => {
   const { id } = req.params;
-  const noteIndex = notes.findIndex(note => note.id === id);
-  if (noteIndex < 0) {
-    return res.status(400).json({ message: 'ID does not exist'});
-  }
-  notes = notes.filter(note => note.id !== id);
-  res.status(204).json({ message: 'User has been deleted'});
+  Note.destroy({
+    where: {
+      id: id
+    }
+  })
+  .then(()=> res.status(200).json({ message: 'Note has been deleted'}))
+  .catch(err => console.log(err));
 };
