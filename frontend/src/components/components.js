f99fc3d1b9cc1ff7bf8d3d9a690a46a1bab6ef2d
carlosf96 <carlos.f.roque96@gmail.com>
2019-11-27 10:58:41 -0600

    fixed white space issue with textarea and restructured note comp

--- frontend/src/components/components.js
+++ frontend/src/components/components.js
@@ -1,19 +1,34 @@
 const createNewNote = note => `
   <li class='list-item col s12 m12 l3' id=${note.id || 'single-note'}>
-    <form id=${note.id + '%'} class='input-forms' onsubmit="saveNoteContent(this)">
+    <form id=${note.id + '%'} class='input-forms' onsubmit='saveNoteContent(this)'>
       <div class='note-title'>
-      <input class='title-input' type='text:not' autocomplete='off' type='submit' id=${note.id + '-title'} placeholder=${note.title || 'Title'}>
-      <i type='text' class='delete-button' id=${note.id} onclick="deleteAndRemoveFromList(this)">x</i>
+        <input 
+          class='title-input' 
+          type='text:not' 
+          autocomplete='off' 
+          type='submit' 
+          id='${note.id + '-title'}'
+          placeholder='${note.title || 'Title'}'/>
+        <i 
+          class='delete-button' 
+          type='text'  
+          id='${note.id}' 
+          onclick='deleteAndRemoveFromList(this)'>x</i>
       </div>
-      <textarea class='body-input flow-text' oninput="saveAfterWhile(this)" type='text' form=${note.id + '%'} id=${note.id + '-body'} placeholder=${note.body || 'Body'}>
-      ${note.body || 'Body'}
-      </textarea>
+      <textarea class='body-input flow-text' 
+        oninput='saveAfterWhile(this)' 
+        type='text' 
+        form='${note.id + '%'}' 
+        id='${note.id + '-body'}' 
+        placeholder='${note.body || 'Body'}'>${note.body || ''}</textarea>
     </form>
   </li> 
   `;
 const offlineIndicator = (() =>`
   <div class='offline-message row'>
-    <p class='col s1 flow-text'>You are offline </p>
-    <i class='col s1 medium material-icons'>cloud_off</i>
+    <p class='col s1 m6 l3'>
+      You are offline
+      <i class='medium material-icons'>cloud_off</i>
+    </p>
   </div>
 `)();
